c7594f2c60bb70d9ece2bc6b4f027fac0ab228b3
Horst Stark <horst.stark@opensky.link>
2022-05-23 12:14:45 +0800

    test: adapt test cases

--- scripts/test/oToken.ts
+++ scripts/test/oToken.ts
@@ -5,24 +5,24 @@ import _ from 'lodash';
 import { expect } from '../helpers/chai';
 import { waitForTx, advanceBlocks, advanceTimeAndBlock, getTxCost, randomAddress } from '../helpers/utils';
 
-import { __setup, setupWithStakingNFT, formatEtherAttrs, formatObjNumbers, checkPoolEquation } from './__setup';
-import { RAY, ONE_YEAR, MAX_UINT_128, MAX_UINT_256, ONE_ETH, POOL_ID, Errors } from '../helpers/constants';
-
-import { ENV } from './__types';
+import { __setup, checkPoolEquation, deposit } from './__setup';
+import { MAX_UINT_128, POOL_ID, Errors, ONE_ETH } from '../helpers/constants';
 
 describe('oToken', function () {
+    let ENV: any;
+    beforeEach(async () => {
+        ENV = await __setup();
+    });
+
     afterEach(async () => {
         await checkPoolEquation();
     });
-    const oneWei = 1;
-    const oneEth = parseEther('1');
 
     it('Check oToken burn 1', async function () {
-        const env: ENV = await setupWithStakingNFT();
-        const { OpenSkyNFT, OpenSkyPool, OpenSkyOToken, nftStaker, deployer, buyer001, buyer002, liquidator } = env;
+        const { OpenSkyOToken, nftStaker } = ENV;
 
         const DEPOSIT_AMOUNT = parseEther('1.2');
-        await nftStaker.OpenSkyPool.deposit('1', 0, { value: DEPOSIT_AMOUNT });
+        await deposit(nftStaker, 1, DEPOSIT_AMOUNT);
         await advanceTimeAndBlock(20);
 
         const INFO: any = {};
@@ -30,7 +30,7 @@ describe('oToken', function () {
         INFO.nftStaker_balance_scaled = await OpenSkyOToken.scaledBalanceOf(nftStaker.address);
         INFO.nftStaker_balance_principle = await OpenSkyOToken.principleBalanceOf(nftStaker.address);
 
-        await nftStaker.OpenSkyPool.withdraw('1', INFO.nftStaker_balance);
+        await nftStaker.OpenSkyPool.withdraw('1', INFO.nftStaker_balance, nftStaker.address);
 
         INFO.nftStaker_balance_after_withdraw = await OpenSkyOToken.balanceOf(nftStaker.address);
 
@@ -39,18 +39,18 @@ describe('oToken', function () {
     });
 
     it('Check oToken totalSupply with sum of user balance [1]', async function () {
-        const { OpenSkyNFT, OpenSkyPool, OpenSkyOToken, nftStaker, deployer, buyer001, buyer002, liquidator } =
-            await setupWithStakingNFT();
+        const { OpenSkyOToken, nftStaker, deployer, buyer001, buyer002, liquidator } =
+            ENV;
 
-        await nftStaker.OpenSkyPool.deposit('1', 0, { value: parseEther('1') });
+        await deposit(nftStaker, 1, parseEther('1'));
         await advanceTimeAndBlock(20);
-        await deployer.OpenSkyPool.deposit('1', 0, { value: parseEther('1.5') });
+        await deposit(nftStaker, 1, parseEther('1.5'));
         await advanceTimeAndBlock(20);
-        await buyer001.OpenSkyPool.deposit('1', 0, { value: parseEther('1.6') });
+        await deposit(nftStaker, 1, parseEther('1.6'));
         await advanceTimeAndBlock(20);
-        await buyer002.OpenSkyPool.deposit('1', 0, { value: parseEther('0.9') });
+        await deposit(nftStaker, 1, parseEther('0.9'));
         await advanceTimeAndBlock(20);
-        await liquidator.OpenSkyPool.deposit('1', 0, { value: parseEther('1.3333') });
+        await deposit(nftStaker, 1, parseEther('1.3333'));
 
         const INFO: any = {};
         INFO.nftStaker = await OpenSkyOToken.balanceOf(nftStaker.address);
@@ -68,18 +68,18 @@ describe('oToken', function () {
     });
 
     it('Check oToken totalSupply with sum of user balance [2]', async function () {
-        const { OpenSkyNFT, OpenSkyPool, OpenSkyOToken, nftStaker, deployer, buyer001, buyer002, liquidator } =
-            await setupWithStakingNFT();
+        const { OpenSkyOToken, nftStaker, deployer, buyer001, buyer002, liquidator } =
+            ENV;
 
         const amount = 1;
-        await nftStaker.OpenSkyPool.deposit('1', 0, { value: amount });
+        await deposit(nftStaker, 1, BigNumber.from(amount));
         await advanceTimeAndBlock(20);
-        await deployer.OpenSkyPool.deposit('1', 0, { value: 2 });
+        await deposit(deployer, 1, BigNumber.from(2));
         await advanceTimeAndBlock(20);
-        await buyer001.OpenSkyPool.deposit('1', 0, { value: 3 });
-        await buyer002.OpenSkyPool.deposit('1', 0, { value: 4 });
+        await deposit(buyer001, 1, BigNumber.from(3));
+        await deposit(buyer002, 1, BigNumber.from(4));
         await advanceTimeAndBlock(20);
-        await liquidator.OpenSkyPool.deposit('1', 0, { value: 5 });
+        await deposit(liquidator, 1, BigNumber.from(5));
 
         const INFO: any = {};
         INFO.nftStaker = await OpenSkyOToken.balanceOf(nftStaker.address);
@@ -95,15 +95,14 @@ describe('oToken', function () {
     });
 
     it('buyer001 deposit 3ETH and transfer 1.5 ether oToken to buyer002', async function () {
-        const env: ENV = await setupWithStakingNFT();
-        const { OpenSkyOToken, buyer001, buyer002 } = env;
+        const { OpenSkyOToken, buyer001, buyer002 } = ENV;
         const INFO: any = {};
 
         INFO.buyer001_0 = await OpenSkyOToken.balanceOf(buyer001.address);
         INFO.buyer002_0 = await OpenSkyOToken.balanceOf(buyer002.address);
         INFO.totalSupply_0 = await OpenSkyOToken.totalSupply();
 
-        await buyer001.OpenSkyPool.deposit('1', 0, { value: parseEther('3') });
+        await deposit(buyer001, 1, parseEther('3'));
 
         INFO.buyer001_1 = await OpenSkyOToken.balanceOf(buyer001.address);
         INFO.totalSupply_1 = await OpenSkyOToken.totalSupply();
@@ -123,31 +122,31 @@ describe('oToken', function () {
 
         expect(INFO.sum_2).to.be.lte(INFO.totalSupply_2);
 
-        expect(await buyer002.OpenSkyPool.withdraw('1', parseEther('1.5')));
+        expect(await buyer002.OpenSkyPool.withdraw('1', parseEther('1.5'), buyer002.address));
     });
 
     it('Check oToken tranfer amount > MAX_UINT_128', async function () {
-        const env: ENV = await setupWithStakingNFT();
-        const { OpenSkyNFT, OpenSkyPool, OpenSkyOToken, nftStaker, deployer, buyer001, buyer002, liquidator } = env;
+        const { OpenSkyNFT, OpenSkyPool, OpenSkyOToken, nftStaker, deployer, buyer001, buyer002, liquidator } = ENV;
         expect(buyer001.OpenSkyOToken.transfer(buyer002.address, MAX_UINT_128.add(1))).to.be.revertedWith(
           Errors.AMOUNT_TRANSFER_OWERFLOW
         );
     });
 
     it('Can change treasury address and accrue interest to new treasury', async function () {
-        const { OpenSkySettings, OpenSkyPool, OpenSkyOToken, buyer001, buyer002 } = await setupWithStakingNFT();
+        const { OpenSkySettings, OpenSkyPool, AAVE_POOL, UnderlyingAsset, OpenSkyOToken, buyer001, buyer002 } = ENV;
         const INFO: any = {};
 
         async function addIncome(amount: BigNumber) {
             await advanceTimeAndBlock(3600 * 24);
-            await OpenSkyPool.updateMoneyMarketIncome(POOL_ID, { value: amount });
+            // await OpenSkyPool.updateMoneyMarketIncome(POOL_ID, { value: amount });
+            await AAVE_POOL.simulateInterestIncrease(UnderlyingAsset.address, OpenSkyOToken.address, amount);
             await OpenSkyPool.updateState(POOL_ID, 0); // add income
             await OpenSkyPool.updateLastMoneyMarketBalance(POOL_ID, 0, 0);
         }
 
         INFO.oldTreasuryAddress = await OpenSkySettings.daoVaultAddress();
         
-        await buyer001.OpenSkyPool.deposit('1', 0, { value: parseEther('1') });
+        await deposit(buyer001, 1, parseEther('1'));
         await addIncome(parseEther('1'));
         INFO.treasury_balance_0 = await OpenSkyOToken.balanceOf(INFO.oldTreasuryAddress);
 
@@ -163,5 +162,41 @@ describe('oToken', function () {
 
         expect(INFO.treasury_balance_2).gt(0);
         // console.log(INFO);
+    });
+});
+
+describe('oToken transfer', function () {
+    let ENV: any;
+    beforeEach(async () => {
+        ENV = await __setup();
+        const { OpenSkyNFT, user001, user002, borrower } = ENV;
+        await deposit(user001, 1, parseEther('3.37332'));
+        await advanceTimeAndBlock(3 * 24 * 3600);
+        await borrower.OpenSkyPool.borrow(
+            1,
+            parseEther('1.5'),
+            365 * 24 * 3600,
+            OpenSkyNFT.address,
+            1,
+            borrower.address
+        );
+        await advanceTimeAndBlock(11 * 24 * 3600);
+        await deposit(user002, 1, parseEther('1.12132'));
+    });
+
+    afterEach(async () => {
+        await checkPoolEquation();
+    });
+
+    it('transfer ', async () => {
+        const { OpenSkyPool, OpenSkyOToken, user001, user003 } = ENV;
+
+        await advanceTimeAndBlock(7 * 24 * 3600);
+
+        await OpenSkyPool.setReserveNormalizedIncome(1, BigNumber.from('3000000000000000000000000000'));
+
+        const reserve = await OpenSkyPool.getReserveData(1); 
+        await user001.OpenSkyOToken.transfer(user003.address, parseEther('1.3222'));
+        expect(await OpenSkyOToken.balanceOf(user003.address)).to.be.equal(parseEther('1.3222'));
     });
 });
