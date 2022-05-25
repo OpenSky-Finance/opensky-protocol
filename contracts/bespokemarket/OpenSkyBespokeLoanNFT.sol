bf1016142ef0fb18bbccb75295ded82e35f752c9
aaron <aaron.young@opensky.link>
2022-05-25 10:35:30 +0800

    feat:add claiming airdrop interfaces to OpenSkyBespokeLoanNFT same as OpenSkyLoan

--- contracts/bespokemarket/OpenSkyBespokeLoanNFT.sol
+++ contracts/bespokemarket/OpenSkyBespokeLoanNFT.sol
@@ -19,7 +19,10 @@ import '../libraries/math/WadRayMath.sol';
 import '../libraries/math/MathUtils.sol';
 import '../interfaces/IOpenSkyIncentivesController.sol';
 import '../interfaces/IOpenSkySettings.sol';
+import '../interfaces/IOpenSkyFlashLoanReceiver.sol';
+import '../interfaces/IACLManager.sol';
 
+import './libraries/BespokeTypes.sol';
 import './interfaces/IOpenSkyBespokeSettings.sol';
 import './interfaces/IOpenSkyBespokeLoanNFT.sol';
 
@@ -39,7 +42,7 @@ contract OpenSkyBespokeLoanNFT is
 
     IOpenSkySettings public immutable SETTINGS;
     IOpenSkyBespokeSettings public immutable BESPOKE_SETTINGS;
-    
+
     uint256 public totalBorrows;
     mapping(address => uint256) public userBorrows;
 
@@ -51,7 +54,12 @@ contract OpenSkyBespokeLoanNFT is
         require(_msgSender() == BESPOKE_SETTINGS.marketAddress(), 'BM_ACL_ONLY_BESPOKR_MARKET_CAN_CALL');
         _;
     }
-    
+
+    modifier onlyAirdropOperator() {
+        IACLManager ACLManager = IACLManager(SETTINGS.ACLManagerAddress());
+        require(ACLManager.isAirdropOperator(_msgSender()), 'BM_ACL_ONLY_AIRDROP_OPERATOR_CAN_CALL');
+        _;
+    }
 
     constructor(
         string memory name,
@@ -93,6 +101,43 @@ contract OpenSkyBespokeLoanNFT is
             );
             incentivesController.handleAction(borrower, userBorrows[borrower], totalBorrows);
         }
+    }
+
+    /// @inheritdoc IOpenSkyBespokeLoanNFT
+    function claimERC20Airdrop(
+        address token,
+        address to,
+        uint256 amount
+    ) external override onlyAirdropOperator {
+        // make sure that params are checked in admin contract
+        IERC20(token).safeTransfer(to, amount);
+        emit ClaimERC20Airdrop(token, to, amount);
+    }
+
+    /// @inheritdoc IOpenSkyBespokeLoanNFT
+    function claimERC721Airdrop(
+        address token,
+        address to,
+        uint256[] calldata ids
+    ) external override onlyAirdropOperator {
+        // make sure that params are checked in admin contract
+        for (uint256 i = 0; i < ids.length; i++) {
+            IERC721(token).safeTransferFrom(address(this), to, ids[i]);
+        }
+        emit ClaimERC721Airdrop(token, to, ids);
+    }
+
+    /// @inheritdoc IOpenSkyBespokeLoanNFT
+    function claimERC1155Airdrop(
+        address token,
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) external override onlyAirdropOperator {
+        // make sure that params are checked in admin contract
+        IERC1155(token).safeBatchTransferFrom(address(this), to, ids, amounts, data);
+        emit ClaimERC1155Airdrop(token, to, ids, amounts, data);
     }
 
     function supportsInterface(bytes4 interfaceId)
--- contracts/bespokemarket/interfaces/IOpenSkyBespokeLoanNFT.sol
+++ contracts/bespokemarket/interfaces/IOpenSkyBespokeLoanNFT.sol
@@ -7,10 +7,53 @@ interface IOpenSkyBespokeLoanNFT is IERC721 {
     event Mint(uint256 indexed tokenId, address indexed recipient);
     event Burn(uint256 tokenId);
 
+    event ClaimERC20Airdrop(address indexed token, address indexed to, uint256 amount);
+    event ClaimERC721Airdrop(address indexed token, address indexed to, uint256[] ids);
+    event ClaimERC1155Airdrop(address indexed token, address indexed to, uint256[] ids, uint256[] amounts, bytes data);
+
     function mint(address borrower) external returns (uint256 loanId);
 
     function burn(uint256 tokenId) external;
 
     function onReceiveNFTFromMarket(address nftAddress, uint256 tokenId) external;
-    
+
+    /**
+     * @notice Claim the ERC20 token which has been airdropped to the loan contract
+     * @param token The address of the airdropped token
+     * @param to The address which will receive ERC20 token
+     * @param amount The amount of the ERC20 token
+     **/
+    function claimERC20Airdrop(
+        address token,
+        address to,
+        uint256 amount
+    ) external;
+
+    /**
+     * @notice Claim the ERC721 token which has been airdropped to the loan contract
+     * @param token The address of the airdropped token
+     * @param to The address which will receive the ERC721 token
+     * @param ids The ID of the ERC721 token
+     **/
+    function claimERC721Airdrop(
+        address token,
+        address to,
+        uint256[] calldata ids
+    ) external;
+
+    /**
+     * @notice Claim the ERC1155 token which has been airdropped to the loan contract
+     * @param token The address of the airdropped token
+     * @param to The address which will receive the ERC1155 tokens
+     * @param ids The ID of the ERC1155 token
+     * @param amounts The amount of the ERC1155 tokens
+     * @param data packed params to pass to the receiver as extra information
+     **/
+    function claimERC1155Airdrop(
+        address token,
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) external;
 }
