699bdfe0d25d286e13e06370f8de6b36aff3fab9
aaron <aaron.young@opensky.link>
2022-06-28 22:08:21 +0800

    fix:add missed events.(OFC-17)

--- contracts/interfaces/IOpenSkyReserveVaultFactory.sol
+++ contracts/interfaces/IOpenSkyReserveVaultFactory.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.10;
 
 interface IOpenSkyReserveVaultFactory {
+    event Create(uint256 indexed reserveId, string name, string symbol, address indexed underlyingAsset);
+
     function create(
         uint256 reserveId,
         string memory name,
