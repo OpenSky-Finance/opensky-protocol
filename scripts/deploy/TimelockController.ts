ce680ffc93b8662f016bea5ad4f8dad945529e32
Horst Stark <horst.stark@opensky.link>
2022-08-02 23:43:45 +0800

    chore: revise timelock deployment script

--- scripts/deploy/TimelockController.ts
+++ scripts/deploy/TimelockController.ts
@@ -21,6 +21,48 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     });
 
     const TimelockController = await ethers.getContract('TimelockController', deployer);
+
+    const ACLManager = await ethers.getContract('ACLManager', deployer);
+    if (await ACLManager.owner() == deployer) {
+        if (!(await ACLManager.isPoolAdmin(TimelockController.address))) {
+            await (await ACLManager.addPoolAdmin(TimelockController.address)).wait();
+        }
+        if (!(await ACLManager.isGovernance(TimelockController.address))) {
+            await (await ACLManager.addGovernance(TimelockController.address)).wait();
+        }
+        await (await ACLManager.transferOwnership(TimelockController.address)).wait();
+    }
+    console.log('ACLManager transfer ownership successfully')
+
+    const OpenSkyWETHGateway = await ethers.getContract('OpenSkyWETHGateway');
+    if (await OpenSkyWETHGateway.owner() == deployer)
+        await (await OpenSkyWETHGateway.transferOwnership(TimelockController.address)).wait();
+    console.log('OpenSkyWETHGateway transfer ownership successfully')
+
+    const OpenSkySettings = await ethers.getContract('OpenSkySettings');
+    if (await OpenSkySettings.owner() == deployer)
+        await (await OpenSkySettings.transferOwnership(TimelockController.address)).wait();
+    console.log('OpenSkyWETHGateway transfer ownership successfully')
+
+    const OpenSkyBespokeSettings = await ethers.getContract('OpenSkyBespokeSettings');
+    if (await OpenSkyBespokeSettings.owner() == deployer)
+        await (await OpenSkyBespokeSettings.transferOwnership(TimelockController.address)).wait();
+    console.log('OpenSkyBespokeSettings transfer ownership successfully')
+
+    const OpenSkyBespokeBorrowNFT = await ethers.getContract('OpenSkyBespokeBorrowNFT');
+    if (await OpenSkyBespokeBorrowNFT.owner() == deployer)
+        await (await OpenSkyBespokeBorrowNFT.transferOwnership(TimelockController.address)).wait();
+    console.log('OpenSkyBespokeBorrowNFT transfer ownership successfully')
+
+    const OpenSkyBespokeLendNFT = await ethers.getContract('OpenSkyBespokeLendNFT');
+    if (await OpenSkyBespokeLendNFT.owner() == deployer)
+        await (await OpenSkyBespokeLendNFT.transferOwnership(TimelockController.address)).wait();
+    console.log('OpenSkyBespokeLendNFT transfer ownership successfully')
+
+    const OpenSkyInterestRateStrategy = await ethers.getContract('OpenSkyInterestRateStrategy');
+    if (await OpenSkyInterestRateStrategy.owner() == deployer)
+        await (await OpenSkyInterestRateStrategy.transferOwnership(TimelockController.address)).wait();
+    console.log('OpenSkyInterestRateStrategy transfer ownership successfully')
 };
 
 export default func;
