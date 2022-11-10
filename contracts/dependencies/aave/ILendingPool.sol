9208278abc0a93f952e42c0146d0929c6c565c6e
Horst Stark <horst.stark@opensky.link>
2022-11-10 19:30:51 +0800

    feat: guarantee for loan

--- contracts/dependencies/aave/ILendingPool.sol
+++ contracts/dependencies/aave/ILendingPool.sol
@@ -70,4 +70,16 @@ interface ILendingPool {
         uint256 amount,
         address to
     ) external returns (uint256);
+
+    function FLASHLOAN_PREMIUM_TOTAL() external view returns (uint256);
+
+    function flashLoan(
+        address receiverAddress,
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata modes,
+        address onBehalfOf,
+        bytes calldata params,
+        uint16 referralCode
+    ) external;
 }
--- scripts/deploy/OpenSkyDutchAuctionPriceOracle.ts
+++ scripts/deploy/OpenSkyDutchAuctionPriceOracle.ts
@@ -7,11 +7,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy } = deployments;
   const { deployer, treasury } = await getNamedAccounts();
   
-  const OpenSkyAuctionPriceOracle = await deploy('OpenSkyDutchAuctionPriceOracle', {
+  await deploy('OpenSkyDutchAuctionPriceOracle', {
     from: deployer,
     args: [],
     log: true,
   });
+
+  const OpenSkyDutchAuctionPriceOracle = await ethers.getContract('OpenSkyDutchAuctionPriceOracle');
+  const OpenSkyDutchAuctionLiquidator = await ethers.getContractAt('OpenSkyDutchAuctionLiquidator', "0x15F3748d261c6fB7a047C2b495f730C88f6d3E31");
+  await OpenSkyDutchAuctionLiquidator.setPriceOracle(OpenSkyDutchAuctionPriceOracle.address);
 };
+
 export default func;
 func.tags = ['OpenSkyDutchAuctionPriceOracle'];
