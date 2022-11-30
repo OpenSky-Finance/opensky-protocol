66ea174f2cf528877313b74ab2d3e4f30df5d8d3
Horst Stark <horst.stark@opensky.link>
2022-12-01 00:54:18 +0800

    refactor: add UserProxies

--- contracts/misc/OpenSkyIncentivesProxy.sol
+++ contracts/misc/OpenSkyIncentivesProxy.sol
@@ -1,28 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import './UserProxy.sol';
+import './UserProxies.sol';
 
-abstract contract OpenSkyIncentivesProxy {
-    mapping(address => UserProxy) public userProxies;
-
-    function _getUserProxy(address user) internal returns (address) {
-        UserProxy userProxy = userProxies[user];
-        if (address(userProxy) == address(0)) {
-            userProxy = new UserProxy();
-            userProxies[user] = userProxy;
-        }
-        return address(userProxy);
-    }
-
-    function _callUserProxy(
-        address user,
-        address target,
-        bytes memory data
-    ) internal {
-        userProxies[user].execute(target, data, 0);
-    }
-
+abstract contract OpenSkyIncentivesProxy is UserProxies {
     function claimRewards(
         address incentivesController,
         address[] calldata assets,
